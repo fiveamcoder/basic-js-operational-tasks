382468a54c4f087d3cf1763331790cd76e883919
mahmudcodes <mahmudcodes@gmail.com>
2024-02-01 14:25:18 +0600

    instruction added

--- task1.js
+++ task1.js
@@ -1,3 +1,23 @@
+/* 
+--------------------------------------------------------
+Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+Sample Input:
+1000
+700
+
+Sample Output:
+300
+-------------------------------------------------------- 
+*/
+
 var money = 1000;
 var orange = 350;
 var apple = 350;
--- task4.js
+++ task4.js
@@ -1,3 +1,16 @@
+/* 
+--------------------------------------------------------
+Task-4
+What will be the result of the following codes:
+
+var a = isNaN(‘11’);
+
+var a = isNaN(2-10);
+
+Explain your answers.
+--------------------------------------------------------
+*/
+
 const a = isNaN('11');
 /* false because this is string but inside a number 
 if i put there '11taka' then true
